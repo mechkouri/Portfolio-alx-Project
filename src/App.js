fa23c21ea6244f820c43a1621ec22f8f817ae0d2
mechkouri <saidmechkouri@gmail.com>
2024-09-19 23:24:00 +0100

    Added comments to App component to explain routing setup and structure

--- src/App.js
+++ src/App.js
@@ -9,20 +9,29 @@ import Blog from "./components/blog/Blog";
 import Contact from "./components/contact/Contact";
 import Footer from "./components/common/footer/Footer";
 import Home from "./components/home/Home";
+
+// Main App Component: Sets up routing and renders the main structure of the app
 function App() {
   return (
     <>
+      {/* Router provides routing functionality to the application */}
       <Router>
+        {/* Header component displayed on all pages */}
         <Header />
+        
+        {/* Switch renders the first matching route */}
         <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/about" exact component={About} />
-          <Route path="/courses" exact component={CourseHome} />
-          <Route path="/team" exact component={Team} />
-          <Route path="/pricing" exact component={Pricing} />
-          <Route path="/journal" exact component={Blog} />
-          <Route path="/contact" exact component={Contact} />
+          {/* Route definitions for different pages */}
+          <Route path="/" exact component={Home} /> {/* Home page route */}
+          <Route path="/about" exact component={About} /> {/* About page route */}
+          <Route path="/courses" exact component={CourseHome} /> {/* Courses page route */}
+          <Route path="/team" exact component={Team} /> {/* Team page route */}
+          <Route path="/pricing" exact component={Pricing} /> {/* Pricing page route */}
+          <Route path="/journal" exact component={Blog} /> {/* Blog page route (renamed from "journal" to "blog") */}
+          <Route path="/contact" exact component={Contact} /> {/* Contact page route */}
         </Switch>
+        
+        {/* Footer component displayed on all pages */}
         <Footer />
       </Router>
     </>
