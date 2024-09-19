c872a3f8680275afd10e4138b738fe8ea88dc178
mechkouri <saidmechkouri@gmail.com>
2024-09-19 23:20:31 +0100

    Added comments to OnlineCourses component for better code clarity and understanding

--- src/components/about/Awrapper.jsx
+++ src/components/about/Awrapper.jsx
@@ -1,16 +1,21 @@
 import React from "react"
 import { awrapper } from "../../dummydata"
 
+// Awrapper Component: Renders a section displaying additional information boxes
 const Awrapper = () => {
   return (
     <>
+      {/* Wrapper section for displaying dynamic content in a grid layout */}
       <section className='awrapper'>
         <div className='container grid'>
-          {awrapper.map((val) => {
+          
+          {/* Loop through awrapper data to dynamically create each box */}
+          {awrapper.map((val, index) => {
             return (
-              <div className='box flex'>
+              // Each box contains an image and related text
+              <div className='box flex' key={index}>
                 <div className='img'>
-                  <img src={val.cover} alt='' />
+                  <img src={val.cover} alt={val.title} />
                 </div>
                 <div className='text'>
                   <h1>{val.data}</h1>
--- src/components/allcourses/OnlineCourses.jsx
+++ src/components/allcourses/OnlineCourses.jsx
@@ -3,19 +3,30 @@ import "./courses.css";
 import { online } from "../../dummydata";
 import Heading from "../common/heading/Heading";
 
+// OnlineCourses Component: Renders a section displaying a list of online courses
 const OnlineCourses = () => {
   return (
     <>
+      {/* Section for displaying online courses */}
       <section className="online">
         <div className="container">
+          
+          {/* Heading for the section */}
           <Heading subtitle="COURSES" title="Browse Our Online Courses" />
+          
+          {/* Grid layout for course items */}
           <div className="content grid3">
+            
+            {/* Loop through online courses data to dynamically create each course item */}
             {online.map((val, index) => (
               <div className="box" key={index}>
+                {/* Course image and hover image */}
                 <div className="img">
                   <img src={val.cover} alt={val.courseName} />
                   <img src={val.hoverCover} alt={`${val.courseName} hover`} className="show" />
                 </div>
+                
+                {/* Course title and description */}
                 <h1>{val.courseName}</h1>
                 <span>{val.course}</span>
               </div>
