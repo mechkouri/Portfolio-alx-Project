ad396d5152d68f9e63b641652ad965d32850e6d9
mechkouri <saidmechkouri@gmail.com>
2024-09-19 23:22:01 +0100

    file updated

--- src/components/allcourses/CoursesCard.jsx
+++ src/components/allcourses/CoursesCard.jsx
@@ -1,59 +1,74 @@
-import React from "react"
-import "./courses.css"
-import { coursesCard } from "../../dummydata"
+import React from "react";
+import "./courses.css";
+import { coursesCard } from "../../dummydata";
 
+// CoursesCard Component: Renders a section displaying a list of course cards
 const CoursesCard = () => {
   return (
     <>
+      {/* Section for displaying course cards */}
       <section className='coursesCard'>
         <div className='container grid2'>
+          
+          {/* Loop through coursesCard data to dynamically create each course card */}
           {coursesCard.map((val) => (
-            <div className='items'>
+            <div className='items' key={val.coursesName}> {/* Use unique key for each item */}
+              
+              {/* Course card content */}
               <div className='content flex'>
                 <div className='left'>
+                  {/* Course image */}
                   <div className='img'>
-                    <img src={val.cover} alt='' />
+                    <img src={val.cover} alt={val.coursesName} /> {/* Added alt text for accessibility */}
                   </div>
                 </div>
+                
                 <div className='text'>
+                  {/* Course title */}
                   <h1>{val.coursesName}</h1>
+                  
+                  {/* Rating section */}
                   <div className='rate'>
                     <i className='fa fa-star'></i>
                     <i className='fa fa-star'></i>
                     <i className='fa fa-star'></i>
                     <i className='fa fa-star'></i>
                     <i className='fa fa-star'></i>
-                    <label htmlFor=''>(5.0)</label>
+                    <label htmlFor=''> (5.0)</label> {/* Rating value */}
                   </div>
+                  
+                  {/* Course details */}
                   <div className='details'>
                     {val.courTeacher.map((details) => (
-                      <>
-                        <div className='box'>
-                          <div className='dimg'>
-                            <img src={details.dcover} alt='' />
-                          </div>
-                          <div className='para'>
-                            <h4>{details.name}</h4>
-                          </div>
+                      <div className='box' key={details.name}> {/* Use unique key for each detail */}
+                        <div className='dimg'>
+                          <img src={details.dcover} alt={details.name} /> {/* Added alt text for accessibility */}
                         </div>
-                        <span>{details.totalTime}</span>
-                      </>
+                        <div className='para'>
+                          <h4>{details.name}</h4> {/* Instructor's name */}
+                        </div>
+                        <span>{details.totalTime}</span> {/* Total course time */}
+                      </div>
                     ))}
                   </div>
                 </div>
               </div>
+              
+              {/* Price section */}
               <div className='price'>
                 <h3>
-                  {val.priceAll} / {val.pricePer}
+                  {val.priceAll} / {val.pricePer} {/* Price details */}
                 </h3>
               </div>
-              <button className='outline-btn'>ENROLL NOW !</button>
+              
+              {/* Enroll button */}
+              <button className='outline-btn'>ENROLL NOW!</button>
             </div>
           ))}
         </div>
       </section>
     </>
-  )
-}
+  );
+};
 
-export default CoursesCard
+export default CoursesCard;
