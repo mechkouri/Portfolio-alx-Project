ba748dfef9c1a0a17760c3848d4db79bfbbd4412
mechkouri <saidmechkouri@gmail.com>
2024-09-19 23:17:36 +0100

    Added comments for better code readability in AboutCard component

--- src/components/about/AboutCard.jsx
+++ src/components/about/AboutCard.jsx
@@ -4,22 +4,35 @@ import "./about.css"
 import { homeAbout } from "../../dummydata"
 import Awrapper from "./Awrapper"
 
+// AboutCard Component: Displays the "About" section of the webpage
 const AboutCard = () => {
   return (
     <>
+      {/* Main section for the About page */}
       <section className='aboutHome'>
         <div className='container flexSB'>
+          
+          {/* Left Column: Displays an image related to the About section */}
           <div className='left row'>
-            <img src='./images/about.webp' alt='' />
+            <img src='./images/about.webp' alt='About us' />
           </div>
+
+          {/* Right Column: Contains heading and dynamic content about the platform benefits */}
           <div className='right row'>
-            <Heading subtitle='LEARN ANYTHING' title='Benefits About Online Learning Expertise' />
+            {/* Heading component with a subtitle and title */}
+            <Heading 
+              subtitle='LEARN ANYTHING' 
+              title='Benefits About Online Learning Expertise' 
+            />
+            
+            {/* Items Section: Dynamically renders each benefit item from the homeAbout data */}
             <div className='items'>
               {homeAbout.map((val) => {
                 return (
-                  <div className='item flexSB'>
+                  // Single Item with image and text (benefit title and description)
+                  <div className='item flexSB' key={val.id}>
                     <div className='img'>
-                      <img src={val.cover} alt='' />
+                      <img src={val.cover} alt={val.title} />
                     </div>
                     <div className='text'>
                       <h2>{val.title}</h2>
@@ -32,6 +45,8 @@ const AboutCard = () => {
           </div>
         </div>
       </section>
+
+      {/* Awrapper component: Could be used to wrap or display additional information */}
       <Awrapper />
     </>
   )
